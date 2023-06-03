b9733e2b9926d5544dcd6f153e8eedba9bb03d23
EL GHAFRAOUI AYOUB <52068209+bbouya@users.noreply.github.com>
2023-06-03 12:52:20 +0100

    Revert "add"
    This reverts commit ae66d99029c51dc3879b295139a3eba1e69f8218.

--- web scrap demos/07/demos/demos/truecar/truecar/main.py
+++
@@ -1,8 +0,0 @@
-# https://stackoverflow.com/questions/21788939/how-to-use-pycharm-to-debug-scrapy-projects
-
-from scrapy import cmdline
-
-
-scrapy_command = 'scrapy crawl truecar -o truecar.csv'
-
-cmdline.execute(scrapy_command.split())
